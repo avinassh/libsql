004f538a3d4e32b35f34eec8d8243933c1ebeffb
Avinash Sajjanshetty <opensource@avi.im>
2024-07-03 21:45:16 +0530

    use async executer

--- libsql-storage-server/benches/benchmarks.rs
+++ libsql-storage-server/benches/benchmarks.rs
@@ -1,11 +1,11 @@
-use criterion::async_executor::FuturesExecutor;
-use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use libsql::{params, Builder, Connection};
 use rand::Rng;
 
-async fn setup_db(url: String, token: String, size: usize) -> Connection {
+async fn setup_db(url: String, token: String, size: u32) -> Connection {
     let db = Builder::new_remote(url, token).build().await.unwrap();
     let conn = db.connect().unwrap();
+    teardown_db(&conn).await;
     conn.execute(
         "CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, value TEXT NOT NULL)",
         (),
@@ -32,40 +32,41 @@ fn random_read_benchmark(c: &mut Criterion) {
     let legacy_wal_url = std::env::var("LIBSQL_LEGACY_URL").unwrap();
     let legacy_wal_token = std::env::var("LIBSQL_LEGACY_AUTH_TOKEN").unwrap();
 
-    let sizes = vec![100, 1000, 100_000, 1_000_000];
+    let sizes: Vec<u32> = vec![100, 200]; //,1000, 10_000];
     let configs = vec![
-        ("legacy", legacy_wal_url, legacy_wal_token),
-        ("durable", durable_wal_url, durable_wal_token),
+        ("legacy", legacy_wal_url.clone(), legacy_wal_token.clone()),
+        ("durable", legacy_wal_url, legacy_wal_token),
+        // ("durable", durable_wal_url, durable_wal_token),
     ];
-
     let mut group = c.benchmark_group("Random Read");
+    group.sample_size(10);
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
     for size in &sizes {
         for config in &configs {
             group.bench_with_input(
                 BenchmarkId::new(format!("random_read_{}", config.0), size),
                 size,
                 |b, &size| {
-                    b.to_async(FuturesExecutor).iter_batched(
-                        || async move {
-                            let conn = setup_db(config.1.clone(), config.2.clone(), size).await;
-                            let id = rand::thread_rng().gen_range(1..=size);
-                            (conn, id)
-                        },
-                        |tuple| async move {
-                            let (conn, id) = tuple;
-                            conn.call(move |conn| {
-                                conn.query_row(
-                                    "SELECT value FROM data WHERE id = ?",
-                                    params![id],
-                                    |row| row.get::<_, String>(0),
-                                )
-                            })
+                    println!("setting up db: {}", size);
+                    let conn = rt.block_on(setup_db(config.1.clone(), config.2.clone(), size));
+                    println!("set up db");
+                    b.to_async(&rt).iter(|| async {
+                        let id = rand::thread_rng().gen_range(1..=size);
+                        println!("got {}", id);
+                        let mut stmt = conn
+                            .prepare("SELECT value FROM data WHERE id = ?")
                             .await
                             .unwrap();
-                            teardown_db(&conn).await;
-                        },
-                        BatchSize::SmallInput,
-                    );
+                        println!("prep done");
+                        let mut rows = stmt.query([id]).await.unwrap();
+                        println!("query done");
+                        let _row = rows.next().await.unwrap().unwrap();
+                        println!("row: {:?}", _row);
+                        println!("tear down done");
+                    });
                 },
             );
         }
